7980181c1d8396312078abc0c34b2139a4ee637e
Martynas Bielskus <bmartynas99@gmail.com>
2024-11-20 18:17:22 +0200

    make: cleanup includes and libs

--- cutils.mk
+++ cutils.mk
@@ -1,43 +1,43 @@
 CUTILS_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
-CUTILS_SRC := $(wildcard $(CUTILS_DIR)src/*.c)
-CUTILS_INC := $(wildcard $(CUTILS_DIR)src/*.h)
-CUTILS_INCLUDE := $(wildcard $(CUTILS_DIR)include/*.h)
-CUTILS_INCLUDE += $(CTEST_INCLUDE)
+CUTILS_SRC := $(CUTILS_DIR)src/
+CUTILS_C := $(wildcard $(CUTILS_SRC)*.c)
+CUTILS_H := $(wildcard $(CUTILS_SRC)*.h)
+CUTILS_HEADERS := $(wildcard $(CUTILS_DIR)include/*.h)
+CUTILS_HEADERS += $(CBASE_HEADERS)
 
-CUTILS_OUTDIR := $(BUILDDIR)bin/cutils/$(ARCH)-$(CONFIG)
+CUTILS_OUTDIR := $(BUILDDIR)bin/$(ARCH)-$(CONFIG)/cutils
 CUTILS_INTDIR := $(CUTILS_OUTDIR)/int/
-CUTILS_OBJ := $(patsubst $(CUTILS_DIR)%.c,$(CUTILS_INTDIR)%.o,$(CUTILS_SRC))
+CUTILS_OBJ := $(patsubst $(CUTILS_SRC)%.c,$(CUTILS_INTDIR)%.o,$(CUTILS_C))
 CUTILS_GCDA := $(patsubst %.o,%.gcda,$(CUTILS_OBJ))
 
-CUTILS_INCLUDES := -I$(CUTILS_DIR)src/ -I$(CUTILS_DIR)include/ -I$(SRCDIR)deps/cbase/include/
+CUTILS_INCLUDES := -I$(CUTILS_DIR)include/ $(CBASE_INCLUDES)
 
-CUTILS_NAME := cutils.a
-CUTILS := $(CUTILS_OUTDIR)/$(CUTILS_NAME)
+CUTILS := $(CUTILS_OUTDIR)/cutils.a
 
 .PHONY: cutils
 cutils: $(CUTILS)
 
-$(CUTILS): $(CTEST) $(CUTILS_OBJ)
+$(CUTILS): $(CUTILS_OBJ)
 	@mkdir -p $(@D)
 	@ar rcs $@ $(CUTILS_OBJ)
 
-$(CUTILS_INTDIR)%.o: $(CUTILS_DIR)%.c $(CUTILS_INC) $(CUTILS_INCLUDE)
+$(CUTILS_INTDIR)%.o: $(CUTILS_SRC)%.c $(CUTILS_H) $(CUTILS_HEADERS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) -c $(CUTILS_INCLUDES) $(CFLAGS) -o $@ $<
+	@$(TCC) -m$(BITS) -c -I$(CUTILS_SRC) $(CUTILS_INCLUDES) $(CFLAGS) -o $@ $<
 
-CUTILS_TEST_SRC := $(wildcard $(CUTILS_DIR)test/*.c)
-CUTILS_TEST_INC := $(wildcard $(CUTILS_DIR)test/*.h)
-CUTILS_TEST_INCLUDE := $(wildcard include/*.h)
-CUTILS_TEST_INCLUDE += $(CUTILS_INCLUDE)
+CUTILS_TEST_SRC := $(CUTILS_DIR)test/
+CUTILS_TEST_C := $(wildcard $(CUTILS_TEST_SRC)*.c)
+CUTILS_TEST_H := $(wildcard $(CUTILS_TEST_SRC)*.h)
+CUTILS_TEST_HEADERS := $(CUTILS_HEADERS) $(CTEST_HEADERS)
 
-CUTILS_TEST_OUTDIR := $(BUILDDIR)bin/cutils_test/$(ARCH)-$(CONFIG)
+CUTILS_TEST_OUTDIR := $(BUILDDIR)bin/$(ARCH)-$(CONFIG)/cutils_test
 CUTILS_TEST_INTDIR := $(CUTILS_TEST_OUTDIR)/int/
-CUTILS_TEST_OBJ := $(patsubst $(CUTILS_DIR)%.c,$(CUTILS_TEST_INTDIR)%.o,$(CUTILS_TEST_SRC))
+CUTILS_TEST_OBJ := $(patsubst $(CUTILS_TEST_SRC)%.c,$(CUTILS_TEST_INTDIR)%.o,$(CUTILS_TEST_C))
 CUTILS_TEST_GCDA := $(patsubst %.o,%.gcda,$(CUTILS_TEST_OBJ))
 
-CUTILS_TEST_INCLUDES := -I$(CUTILS_DIR)test/ -I$(CUTILS_DIR)include/ -I$(SRCDIR)deps/cbase/include/ -I$(SRCDIR)deps/ctest/include/
-CUTILS_TEST_LIBS := -L$(CUTILS_OUTDIR) -l:$(CUTILS_NAME) -L$(CTEST_OUTDIR) -l:$(CTEST_NAME) -L$(CBASE_OUTDIR) -l:$(CBASE_NAME)
+CUTILS_TEST_INCLUDES := $(CUTILS_INCLUDES) $(CTEST_INCLUDES) 
+CUTILS_TEST_LIBS := $(CUTILS) $(CTEST) $(CBASE)
 
 CUTILS_TEST := $(CUTILS_TEST_OUTDIR)/cutils_test
 
@@ -46,10 +46,10 @@ cutils_test: $(CUTILS_TEST)
 	@rm -rf $(CUTILS_GCDA) $(CUTILS_TEST_GCDA)
 	@$(CUTILS_TEST)
 
-$(CUTILS_TEST): $(CUTILS) $(CUTILS_TEST_OBJ)
+$(CUTILS_TEST): $(CUTILS_TEST_OBJ) $(CUTILS_TEST_LIBS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) $(LDFLAGS) -o $@ $(CUTILS_TEST_OBJ) $(CUTILS_TEST_LIBS)
+	@$(TCC) -m$(BITS) $(LDFLAGS) -o $@ $(CUTILS_TEST_OBJ) $(patsubst %,-L%,$(dir $(CUTILS_TEST_LIBS))) $(patsubst %,-l:%,$(notdir $(CUTILS_TEST_LIBS)))
 
-$(CUTILS_TEST_INTDIR)%.o: $(CUTILS_DIR)%.c $(CUTILS_TEST_INC) $(CUTILS_TEST_INCLUDE)
+$(CUTILS_TEST_INTDIR)%.o: $(CUTILS_TEST_SRC)%.c $(CUTILS_TEST_H) $(CUTILS_TEST_HEADERS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) -c $(CUTILS_TEST_INCLUDES) $(CFLAGS) -o $@ $<
+	@$(TCC) -m$(BITS) -c -I$(CUTILS_TEST_SRC) $(CUTILS_TEST_INCLUDES) $(CFLAGS) -o $@ $<
